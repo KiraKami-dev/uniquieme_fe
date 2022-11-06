a8d4a01748407f09bcb85408294caf6b6c5f234f
Akshat Khair <akshatkhair33@gmail.com>
2022-11-06 13:42:41 +0530

    Socialdone

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-1deg)' },
+          '50%': { transform: 'rotate(1deg)' },
+        }
+      },
       animation: {
         loop : 'leftinft 1s linear Infinite',
+        wiggle: 'wiggle 0.2s ease-in-out infinite',
       },
       colors:{
         'Backblue' : '#7FD8E0',
@@ -12,6 +19,7 @@ module.exports = {
       backgroundImage:{
         'sea': "url('/img/sea.png')",
       }
+      
     },
   },
   plugins: [],
