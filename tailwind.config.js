8fe5636f0097ca2d67d373de263939dee34b4abc
Kira <akshatkhair33@gmail.com>
2022-11-06 16:16:48 +0530

    fontadd1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,34 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
+      fontFamily:{
+        another : ["another"],
+      },
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-1deg)' },
           '50%': { transform: 'rotate(1deg)' },
-        }
+        },
+        type: {
+          '0%': { width: '0ch' },
+          '5%, 10%': { width: '1ch' },
+          '15%, 20%': { width: '2ch' },
+          '25%, 30%': { width: '3ch' },
+          '35%, 40%': { width: '4ch' },
+          '45%, 50%': { width: '5ch' },
+          '55%, 60%': { width: '6ch' },
+          '65%, 70%': { width: '7ch' },
+          '75%, 80%': { width: '8ch' },
+          '85%, 90%': { width: '9ch' },
+          '95%': { width: '10ch' },
+        },
       },
       animation: {
         loop : 'leftinft 1s linear Infinite',
         wiggle: 'wiggle 0.2s ease-in-out infinite',
+        cursor: 'cursor .6s linear infinite alternate',
+        type: 'type 1.8s ease-out .8s 1 normal both',
+        'type-reverse': 'type 1.8s ease-out 0s infinite alternate-reverse both',
       },
       colors:{
         'Backblue' : '#7FD8E0',
