dda2e3bb411649a0748947b44151732d9ab9118f
Akshat Khair <akshatkhair33@gmail.com>
2022-11-06 17:39:47 +0530

    Animation&font remaining

--- unqiueme.js
+++ unqiueme.js
@@ -12,3 +12,18 @@ function showsocial() {
       name.innerHTML = "Show more";
     }
   }
+
+  function showsdomain() {
+    var x = document.getElementById("socialextra3");
+    var y = document.getElementById("socialextra4");
+    var name = document.getElementById("domainbutton")
+    if (x.style.display === "none" && y.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "block";
+      name.innerHTML = "Show less";
+    } else {
+      x.style.display = "none";
+      y.style.display = "none";
+      name.innerHTML = "Show more";
+    }
+  }
